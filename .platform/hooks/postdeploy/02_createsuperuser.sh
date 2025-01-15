0edc6072abc6559decc96892ceacd252cb9bd0c9
kcspex <kcspex@gmail.com>
2025-01-15 12:12:40 +0100

    updated

--- .platform/hooks/postdeploy/02_createsuperuser.sh
+++ .platform/hooks/postdeploy/02_createsuperuser.sh
@@ -11,7 +11,7 @@ echo "Creating Django superuser..."
 source "$PYTHONPATH/activate" && {
 
     # Run superuser creation only on the leader instance
-    if [[ $EB_IS_COMMAND_LEADER == "true" ]]; then
+    # if [[ $EB_IS_COMMAND_LEADER == "true" ]]; then
         python manage.py shell <<EOF
     from accounts.models import Account
     Account.objects.create_superuser(
@@ -22,8 +22,8 @@ source "$PYTHONPATH/activate" && {
         '123456'
     )
     EOF
-    else
-        echo "This instance is not the leader. Skipping superuser creation."
-    fi
+    # else
+        # echo "This instance is not the leader. Skipping superuser creation."
+    # fi
 
 }
